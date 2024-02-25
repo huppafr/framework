5fe9764c4f22c37175671e289d26a43cfafe08b4
hyuppenen artem <hyuppenen.a@raidix.com>
2024-02-25 20:02:17 +0300

    Added cmd result class

--- framework/cmd_mappers/BashResult.py
+++ framework/cmd_mappers/BashResult.py
@@ -4,18 +4,9 @@ logger = logging.getLogger(__name__)
 
 
 class BashResult:
-    def __init__(self, cmd: str, output: str, exit_code: int, ip_addr: str) -> None:
-        """
-        Initialize a BashResult instance.
-
-        Args:
-            cmd (str): Command executed.
-            output (str): Output produced by the command.
-            exit_code (int): Exit code returned by the command.
-            ip_addr (str): IP address of the target where command was executed.
-        """
+    def __init__(self, cmd: str, output: str, exit_code: int, ip_addr: str):
         self.cmd = cmd
-        self.output = output.strip()
+        self.output = output.strip() if isinstance(output, str) else output
         self.exit_code = exit_code
         self.ip_addr = ip_addr
 
@@ -26,9 +17,9 @@ class BashResult:
         return self.exit_code != 0
 
     def __repr__(self) -> str:
-        return f'[{self.ip_addr}] : [{self.cmd}] : [{self.exit_code}]' f'\n{self.output}'
+        return f"[{self.ip_addr}] : [{self.cmd}] : [{self.exit_code}] \n{self.output}"
 
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other):
         return (
             isinstance(other, BashResult)
             and self.exit_code == other.exit_code
